635c53f0129d07064875d2e1d43fe84e066e2963
Lee Chun On <jeffreyleeon@gmail.com>
2016-07-17 13:38:49 +0800

    First commit

---
+++ Assets/Request.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Request : MonoBehaviour {
+	
+	void Start () {
+		string url = "http://159.203.97.81/chefs";
+		WWW www = new WWW(url);
+		StartCoroutine(WaitForRequest(www));
+	}
+
+	IEnumerator WaitForRequest(WWW www)
+	{
+		yield return www;
+
+		// check for errors
+		if (www.error == null)
+		{
+			Debug.Log("WWW Ok!: " + www.data);
+		} else {
+			Debug.Log("WWW Error: "+ www.error);
+		}    
+	}
+}
